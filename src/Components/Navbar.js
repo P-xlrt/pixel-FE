c430f81457bc1af811f40835b329f437a875c0ff
jull92 <julia.kata92@gmail.com>
2022-03-25 10:49:50 +0000

    navbar doesnt work, still fixing it

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,17 +1,27 @@
 import { Link } from "react-router-dom";
+import { usePopper } from "react-popper";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Dropdown} from "react-bootstrap"
 import "../styling/navbar.css";
+
 export const Navbar = () => {
   return (
-    <nav className='main_nav'>
-      <Link to='/logo'>
-        <img src='' alt='logo' />
-      </Link>
-      <Link to='/settings'>Settings</Link>
-      <Link to='/login'>Login</Link>
-      <Link to='/gallery'>Gallery</Link>
-      <Link to='/create'>Create</Link>
-      <Link to='/profile'>Profile</Link>
-      <Link to='/settings'>Settings</Link>
-    </nav>
+    // <nav className='main_nav'>
+    //   <Link to='/logo'>
+    //     <img src='' alt='logo' />
+    //   </Link>
+    //   <Link to='/create'>Create</Link>
+    //   <Link to='/login'>Login</Link>
+    //   <Dropdown>
+    //     <Dropdown.Toggle variant="success" id="userDropdown">Menu</Dropdown.Toggle>
+    //     <Dropdown.Menu variant="dark">
+    //       <Dropdown.Item><Link to='/gallery'>Public gallery</Link></Dropdown.Item>
+    //       <Dropdown.Item><Link to='/profile'>Profile</Link></Dropdown.Item>
+    //       <Dropdown.Item><Link to='/settings'>Settings</Link></Dropdown.Item>
+    //       <Dropdown.Divider />
+    //       <Dropdown.Item style="color:#8A1953"><Link to='/'>Log Out</Link></Dropdown.Item>
+    //     </Dropdown.Menu>
+    //   </Dropdown>
+    // </nav>
   );
 };
