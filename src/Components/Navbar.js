58bae470d1ca97ad1acdeb4f8c5b79000c325356
Nateldn <32700665+Nateldn@users.noreply.github.com>
2022-03-25 17:22:27 +0000

    Add login - tweak style

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import {Dropdown} from "./Dropdown"
+import { Dropdown } from "./Dropdown";
 import "../styling/navbar.css";
 
 export const Navbar = () => {
@@ -25,31 +25,36 @@ export const Navbar = () => {
 
   return (
     <>
-      <nav className='main-nav'>
-        <Link to='/gallery' className='navbar-logo'>WebsiteLogo</Link>
+      <nav className='navbar'>
+        <Link to='/gallery' className='navbar-logo'>
+          WebsiteLogo
+        </Link>
         <div className='menu-icon' onClick={handleClick}>
-          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          <i className={click ? "fas fa-times" : "fas fa-bars"} />
         </div>
-        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
           <li className='nav-item'>
             <Link to='/create' className='nav-links' onClick={closeMobileMenu}>
               Create
-              </Link>
+            </Link>
           </li>
           <li className='nav-item'>
             <Link to='/login' className='nav-links' onClick={closeMobileMenu}>
               Log In or Sign Up
-              </Link>
+            </Link>
           </li>
-          <li className='nav-item'
+          <li
+            className='nav-item'
             onMouseEnter={onMouseEnter}
-            onMouseLeave={onMouseLeave}>
-            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-              Menu
-              </Link>
-              {dropdown && <Dropdown />}
+            onMouseLeave={onMouseLeave}
+            onClick={closeMobileMenu}
+            id='nav-links'
+          >
+            {/* <Link to='/' className='nav-links' onClick={closeMobileMenu}> */}
+            Menu <i className='fas fa-caret-down' />
+            {/* </Link> */}
+            {dropdown && <Dropdown />}
           </li>
-
         </ul>
         {/* <Link to='/create'>Create</Link>
         <Link to='/login'>Login</Link>
@@ -61,7 +66,6 @@ export const Navbar = () => {
     </>
   );
 };
-
 
 // export const Navbar = () => {
 //   return (
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,49 @@
-// here we'll connect our front to back end
+export const createUser = async (username, email, pass, setter) => {
+  try {
+    const response = await fetch(`${process.env.REACT_APP_REST_API}user`, {
+      method: "POST", // all HTTP e.g. get, post, put  methods must be in string and uppercase letters
+      headers: { "Content-Type": "application/json" }, // headers should be structured in JSON format
+      body: JSON.stringify({
+        username: username,
+        email: email,
+        password: pass,
+      }),
+    });
+    const data = await response.json();
+    setter(data.user);
+    localStorage.setItem("myToken", data.token);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const login = async (username, pass, setter) => {
+  try {
+    const response = await fetch(`${process.env.REACT_APP_REST_API}login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: username,
+        password: pass,
+      }),
+    });
+    const data = await response.json();
+    setter(data.user);
+    localStorage.setItem("myToken", data.token);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const tokenLogin = async (setter) => {
+  try {
+    const response = await fetch(`${process.env.REACT_APP_REST_API}user`, {
+      method: "GET",
+      headers: { Authorisation: `Bearer ${localStorage.getItem("myToken")}` },
+    });
+    const data = await response.json();
+    setter(data.user);
+  } catch (error) {
+    console.log(error);
+  }
+};
