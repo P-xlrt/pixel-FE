d3d45417b3caf43fa7a92635fcb05b86ffad6359
jull92 <julia.kata92@gmail.com>
2022-03-28 16:36:18 +0100

    added user settings page and styling, made the navbar wider, dropdown needs to be flipped to open to the left

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -1,9 +1,33 @@
 // User settings page: Update Email, Update Password, Delete Account, Add/Change Image and Log-out
+import "../styling/settings.css";
 
 export const Settings = () => {
   return (
-    <div>
-      <h1>Settings</h1>
+    <div className="settingsContainer">
+      <div className="settingsTitle">
+        <h1>Settings</h1>
+      </div>
+      <div className="settingsOther">
+        <div className="settings">
+          <h2>Set lading page</h2>
+          <input />
+        </div>
+        <div className="settings">
+          <h2>Change email</h2>
+          <input />
+        </div>
+        <div className="settings">
+          <h2>Change password</h2>
+          <input />
+        </div>
+        <div className="settings" id="deleteAccount">
+          <h2>Delete account</h2>
+        </div>
+      </div>
+      <div className="settingsPic">
+        <img src="" alt="user profile image" id="userProfileImage"/>
+        <h2>Change profile image</h2>
+      </div>
     </div>
   );
 };
