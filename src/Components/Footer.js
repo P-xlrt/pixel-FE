35a8a5ce7774756c80c5ced08b143b2c4d937279
jull92 <julia.kata92@gmail.com>
2022-03-28 15:41:32 +0100

    added gallery function and styling, changed class in footer.js to className since it was throwing errors

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,6 @@
 //  Footer -
 import { Link } from "react-router-dom";
-import "../styling/footer-style.css";
+import "../styling/footer.css";
 export const Footer = () => {
   return (
     <section className='footer_wrap'>
@@ -13,22 +13,22 @@ export const Footer = () => {
         <ul>
           <li>
             <a href='#'>
-              <i class='fab fa-facebook fa-lg'></i>
+              <i className='fab fa-facebook fa-lg'></i>
             </a>
           </li>
           <li>
             <a href='#'>
-              <i class='fab fa-instagram fa-lg'></i>
+              <i className='fab fa-instagram fa-lg'></i>
             </a>
           </li>
           <li>
             <a href='#'>
-              <i class='fab fa-twitter fa-lg'></i>
+              <i className='fab fa-twitter fa-lg'></i>
             </a>
           </li>
           <li>
             <a href='#'>
-              <i class='fab fa-pinterest fa-lg'></i>
+              <i className='fab fa-pinterest fa-lg'></i>
             </a>
           </li>
         </ul>
