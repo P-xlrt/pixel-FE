fce6e3ee8eecca4a174f26e78cbbd72836320cf1
Damien R <damien.rembert@gmail.com>
2022-03-28 19:59:13 +0100

    temp gallery

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,18 +1,43 @@
+//@ts-check
+
+
 // Gallery - Comprising both public and private galleries. Grid of images displayed to users
 import "../styling/gallery.css";
+import { grabImages } from "../utils/imgRequests";
+import { useEffect, useState } from "react";
+
+
+
+
+
+
+
 
 export const Gallery = () => {
+  const [images, setImages] = useState([]);
+  const [totalImgQty, setTotalImgQty] = useState([]);
+
+  // export const grabImages = async (setImages, setTotalImgQty, itemsPerPage, pageNumber, targetUser) => {
+
+
+  useEffect(() => {
+    grabImages(setImages, setTotalImgQty, 20, 1, "all");
+  }, []);
+  
+
+
   return (
     <div className="galleryContainer">
       <h1>Gallery</h1>
       <div className="galleryImage">
-        {/* {galleryImages.map((img, index) => {
+        {images.map((imgObj, index) => {
           return (
             <div key={index}>
-              <img src={img.img.path} alt={img.img.name}/>
+              <p>{imgObj.title}</p>
+              <img src={imgObj.img} alt="hey"/>
             </div>
           );
-        })} */}
+        })}
       </div>
     </div>
   );
