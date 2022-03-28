374791cb048a64e23b3ca9fcb055367b37f0958d
Damien Rembert <damien.rembert@gmail.com>
2022-03-28 23:23:38 +0100

    very messy but working gallery

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -6,23 +6,30 @@ import "../styling/gallery.css";
 import { grabImages } from "../utils/imgRequests";
 import { useEffect, useState } from "react";
 
+// we need an api request to look up a name and return their id and avatar
 
+// https://stackoverflow.com/questions/4773966/drawing-an-image-from-a-data-url-to-a-canvas
 
+// https://reactrouter.com/docs/en/v6/getting-started/tutorial#listing-the-invoices
+// https://reactrouter.com/docs/en/v6/getting-started/concepts#outlets
 
-
-
-
+// on-click of creator's name use setAuthor and redirect
 
 export const Gallery = () => {
   const [images, setImages] = useState([]);
-  const [totalImgQty, setTotalImgQty] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalImgQty, setTotalImgQty] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(9);
+  const [author, setAuthor] = useState("all");
 
   // export const grabImages = async (setImages, setTotalImgQty, itemsPerPage, pageNumber, targetUser) => {
 
 
   useEffect(() => {
-    grabImages(setImages, setTotalImgQty, 20, 1, "all");
+    grabImages(setImages, setTotalImgQty, itemsPerPage, currentPage, author);
   }, []);
+
+  let pageQty = Math.floor(totalImgQty / itemsPerPage);
   
 
 
@@ -34,7 +41,7 @@ export const Gallery = () => {
           return (
             <div key={index}>
               <p>{imgObj.title}</p>
-              <img src={imgObj.img} alt="hey"/>
+              <img src={imgObj.img} alt="a beautiful creation"/>
             </div>
           );
         })}
