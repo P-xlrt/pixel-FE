b8e9dd485641b94b1e67cf3027b5a335ccbc03a1
jull92 <julia.kata92@gmail.com>
2022-03-29 11:18:03 +0100

    fixed styling on the settings page, pushed footer to the bottom of the page, added landing page, done some styling to the gallery, navbar, settings, footer, dropdown

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,8 +1,16 @@
 // The first pages visitor see when the arriving
+import {Gallery} from "./Gallery"
+import "../styling/gallery.css";
+
 export const Landing = () => {
   return (
     <div>
-      <h1>Welcome to p:xlr</h1>
+      <div className="landingContainer">
+        <h1>Welcome to P:xlrt</h1>
+        <h2>P:xlrt is creative platform for aficionados of pixel art. P:xlr allows professional and amateur digital artists to design, save and showcase their pixel art.</h2>
+      </div>
+      <Gallery/>
     </div>
+    
   );
 };
