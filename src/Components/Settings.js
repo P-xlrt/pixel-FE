4bc215a4b8a8dde30138ee9bf5abc51ecd216a27
Nateldn <32700665+Nateldn@users.noreply.github.com>
2022-03-29 16:55:56 +0100

    Add -  import user update and delete calls

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -1,31 +1,32 @@
 // User settings page: Update Email, Update Password, Delete Account, Add/Change Image and Log-out
 import "../styling/settings.css";
+import { deleteUser, updateUser } from "../utils";
 
 export const Settings = () => {
   return (
-    <div className="settingsContainer">
-      <div className="settingsTitle">
+    <div className='settingsContainer'>
+      <div className='settingsTitle'>
         <h1>Settings</h1>
       </div>
-      <div className="settingsOther">
-        <div className="settings">
+      <div className='settingsOther'>
+        <div className='settings'>
           <h2>Set lading page</h2>
           <input />
         </div>
-        <div className="settings">
+        <div className='settings'>
           <h2>Change email</h2>
           <input />
         </div>
-        <div className="settings">
+        <div className='settings'>
           <h2>Change password</h2>
           <input />
         </div>
-        <div className="settings" id="deleteAccount">
+        <div className='settings' id='deleteAccount'>
           <h2>Delete account</h2>
         </div>
       </div>
-      <div className="settingsPic">
-        <img src="" alt="user profile image" id="userProfileImage"/>
+      <div className='settingsPic'>
+        <img src='' alt='user profile image' id='userProfileImage' />
         <h2>Change profile image</h2>
       </div>
     </div>
