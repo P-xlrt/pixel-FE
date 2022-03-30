2dfa8f28b91b9ba35a06af61402808dea5938687
jull92 <julia.kata92@gmail.com>
2022-03-30 10:06:23 +0100

    added meet the team page, added it to the browser routes, needs finishing tho

---
+++ src/Components/Team.js
@@ -0,0 +1,38 @@
+import "../styling/team.css";
+
+export const Team = () => {
+    return (
+        <div className="teamContainer">
+            <div className="teamMember">
+                <h2>Liam</h2>
+                <img />
+                <p></p>
+            </div>
+            <div className="teamMember">
+                <h2>Nate</h2>
+                <img />
+                <p></p>
+            </div>
+            <div className="teamMember">
+                <h2>Damien</h2>
+                <img />
+                <p></p>
+            </div>
+            <div className="teamMember">
+                <h2>Terry</h2>
+                <img />
+                <p></p>
+            </div>
+            <div className="teamMember">
+                <h2>Peter</h2>
+                <img />
+                <p></p>
+            </div>
+            <div className="teamMember">
+                <h2>Julia</h2>
+                <img />
+                <p></p>
+            </div>
+        </div>
+    )
+}
