f2fb2d48dbcb739c77dfc3bc8988982c69a2809c
Damien R <damien.rembert@gmail.com>
2022-04-08 12:41:00 +0100

    added Link import - maybe fixed?

--- src/Components/Logout.js
+++ src/Components/Logout.js
@@ -1,34 +1,34 @@
-import { Navigate } from "react-router-dom";
-// import { useEffect } from "react";
+import { Navigate, Link } from "react-router-dom";
+import { useEffect } from "react";
 
 import { logout } from "../utils";
 import "../styling/login.css";
 
+// export const Logout = () => {
 export const Logout = ({ user, setUser }) => {
 
-  // useEffect(() => {
-  //   if (localStorage.key("myToken")) {
-  //       logout(setUser);
-  //   }
-  // });
+  useEffect(() => {
+    if (localStorage.key("myToken")) {
+        logout(setUser);
+    }
+  }, []);
 
-  if (localStorage.key("myToken")) {
-    logout(setUser);
-  }
+  // if (localStorage.key("myToken")) {
+  //   logout(setUser);
+  // }
 
 
 
   
   return (
     <div>
-      { user ?  <h1>${user} You're being logged out...</h1> : <h1>You've been logged out...</h1> }
+      { !user && <Navigate to='/gallery' /> }
+      { user ?  <h1>${user}, You're being logged out...</h1> : <h1>You've been logged out...</h1> }
       <Link to='/gallery'>Go back to the public gallery!</Link>
+
       <Link to='/create'>Go create beautiful art!</Link>
+
       <Link to='/login' >Go to the Login/Sign-up page!</Link>
-
-
-
-      {/* {!user && <Navigate to='/gallery' />} */}
 
     </div>
   );
